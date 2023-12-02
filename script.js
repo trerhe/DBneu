5c20588fc9602ca00b28d3b13646dfa1f12cd4b0
Vae Tiolamon <72586932+Vae2002@users.noreply.github.com>
2023-12-02 19:44:03 +0100

    new

---
+++ script.js
@@ -0,0 +1,41 @@
+<<<<<<< HEAD
+
+// Define your model here
+const myModel = {
+  // Properties
+  property1: 'value1',
+  property2: 'value2',
+
+  // Methods
+  method1() {
+    // Code for method1
+  },
+  method2() {
+    // Code for method2
+  }
+};
+
+// Use your model here
+console.log(myModel.property1);
+myModel.method1();
+=======
+
+// Define your model here
+const myModel = {
+  // Properties
+  property1: 'value1',
+  property2: 'value2',
+
+  // Methods
+  method1() {
+    // Code for method1
+  },
+  method2() {
+    // Code for method2
+  }
+};
+
+// Use your model here
+console.log(myModel.property1);
+myModel.method1();
+>>>>>>> c765253437647f99337f15ad2b631e987783eb71
